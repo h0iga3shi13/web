f3dba74de14896119f5a1a24ab7e27031518830c
unknown <>
2017-01-28 18:26:42 +0900

    first commit

---
+++ readme.txt
@@ -0,0 +1,1 @@
+herokuでSpringを実行
---
+++ src/main/java/com/sbc/web/AppController.java
@@ -0,0 +1,13 @@
+package com.sbc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+}
